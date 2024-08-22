a0dd02675f1586536472f91e73d5bc452b04f433
Andrei Savu <savu.andrei@gmail.com>
2024-08-22 08:25:51 -0700

    Update test_chess.py to use UCI format for moves
    * Replace `board.push_san("e4")` with `board.push(chess.Move.from_uci("e2e4"))`
    * Replace `assert board.san(board.peek()) == "e4"` with `assert board.peek() == chess.Move.from_uci("e2e4")`

--- tests/test_chess.py
+++ tests/test_chess.py
@@ -2,5 +2,5 @@ import chess
 
 def test_chess_opening_move():
     board = chess.Board()
-    board.push_san("e4")
-    assert board.san(board.peek()) == "e4"
+    board.push(chess.Move.from_uci("e2e4"))
+    assert board.peek() == chess.Move.from_uci("e2e4")
